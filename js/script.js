55eeda98b0518c9bf751d307dca16f9b47645f5a
Jone Bulande <JoneBulande@gmail.com>
2020-08-30 20:57:35 +0200

    Cronometro

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const tempo = document.querySelector('.tempo'),
+			iniciar = document.querySelector('.iniciar'),
+			pausar = document.querySelector('.pausar'),
+			resetar = document.querySelector('.resetar');
+
+iniciar.addEventListener('click', iniciarTempo)
+pausar.addEventListener('click', pausarTempo)
+resetar.addEventListener('click', resetarTempo)
+
+let timer;
+let seg = 0;
+let min = 0;
+let hor = 0;
+
+function iniciarTempo(){
+	iniciar.setAttribute('disabled', '');
+	timer = setInterval(() => {
+		if(seg === 59) {
+			seg = 0;
+			min += 1;
+		}
+		else if(min === 59){
+			seg = 0;
+			min = 0;
+			hor += 1;
+		}
+
+		tempo.innerText = `${hor}:${min}:${seg++}`;
+	},1000);
+}
+
+function pausarTempo(){
+	iniciar.removeAttribute('disabled', '');
+	clearInterval(timer);
+}
+function resetarTempo(){
+}
